fb18321287b56794f741e233c710e2a2efbf3a9a
Poulami-01 <56344602+Poulami-01@users.noreply.github.com>
2020-10-01 10:57:54 +0530

    Update add.asm

--- add.asm
+++ add.asm
@@ -1,14 +1,14 @@
 
 ;<Add two numbers>
-mvi c,00h
-lda 2000h
-mov b,a
-lda 2001h
-add b
+mvi b,00h
+lda 8000h
+mov c,a
+lda 8001h
+add c
 jnc skip
-inr c
-skip: sta 2002h
-sta 2002h
-mov a,c
+inr b
+skip: sta 8002h
+sta 8002h
+mov a,b
 sta 2003h
 hlt 
